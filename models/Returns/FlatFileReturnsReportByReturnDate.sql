f8a5dbeb2228f03d1e8a276d5e80fb8bef6487d8
sardev <developer@sarasanalytics.com>
2022-12-01 11:55:39 +0530

    currency column change & model name fix

--- models/Returns/FlatFileReturnsReportByReturnDate.sql
+++ models/Returns/FlatFileReturnsReportByReturnDate.sql
@@ -104,7 +104,7 @@ where lower(table_name) like '%flatfilereturnsreportbyreturndate'
         Category,
         {% if var('currency_conversion_flag') %}
             case when c.value is null then 1 else c.value end as conversion_rate,
-            case when c.from_currency_code is null then currency else c.from_currency_code end as conversion_currency, 
+            case when c.from_currency_code is null then a.Currency_code else c.from_currency_code end as conversion_currency, 
         {% else %}
             cast(1 as decimal) as conversion_rate,
             cast(null as string) as conversion_currency, 
