464581c0dbdbb358db94198cfae89b9a16bff56a
SarasAnalytics-Daton <developer@sarasanalytics.com>
2023-11-03 05:58:46 +0000

    catalog items & fabamazonfulfilledshipment

--- models/AmazonSellerCentral/CatalogItems.sql
+++ models/AmazonSellerCentral/CatalogItems.sql
@@ -4,99 +4,72 @@
     {{ config( enabled = False ) }}
 {% endif %}
 
-    {% if is_incremental() %}
-    {%- set max_loaded_query -%}
-    select coalesce(max(_daton_batch_runtime) - 2592000000,0) from {{ this }}
-    {% endset %}
 
-    {%- set max_loaded_results = run_query(max_loaded_query) -%}
+{% set relations = dbt_utils.get_relations_by_pattern(
+schema_pattern=var('raw_schema'),
+table_pattern=var('CatalogItems_tbl_ptrn'),
+exclude=var('CatalogItems_tbl_exclude_ptrn'),
+database=var('raw_database')) %}
 
-    {%- if execute -%}
-    {% set max_loaded = max_loaded_results.rows[0].values()[0] %}
+{% for i in relations %}
+    {% if var('get_brandname_from_tablename_flag') %}
+        {% set brand =replace(i,'`','').split('.')[2].split('_')[var('brandname_position_in_tablename')] %}
     {% else %}
-    {% set max_loaded = 0 %}
-    {%- endif -%}
+        {% set brand = var('default_brandname') %}
     {% endif %}
 
-    {% set table_name_query %}
-    {{set_table_name('%catalogitems')}}    
-    {% endset %}   
-
-
-    {% set results = run_query(table_name_query) %}
-    {% if execute %}
-    {# Return the first column #}
-    {% set results_list = results.columns[0].values() %}
+    {% if var('get_storename_from_tablename_flag') %}
+        {% set store =replace(i,'`','').split('.')[2].split('_')[var('storename_position_in_tablename')] %}
     {% else %}
-    {% set results_list = [] %}
+        {% set store = var('default_storename') %}
     {% endif %}
 
-
-    {% for i in results_list %}
-        {% if var('get_brandname_from_tablename_flag') %}
-            {% set brand =i.split('.')[2].split('_')[var('brandname_position_in_tablename')] %}
-        {% else %}
-            {% set brand = var('default_brandname') %}
+    select 
+    '{{brand|replace("`","")}}' as brand,
+    '{{store|replace("`","")}}' as store,
+    {{ timezone_conversion("RequeststartDate") }} as RequeststartDate,
+    {{ timezone_conversion("RequestendDate") }} as RequeststartDate,
+    coalesce(ReferenceASIN,'N/A') as ReferenceASIN,
+    sellingPartnerId,
+    marketplaceName,
+    a.marketplaceId,
+    asin,
+    {{extract_nested_value("salesRanks","marketplaceId","string")}} as salesRanks_marketplaceId,
+    {{extract_nested_value("classificationRanks","classificationId","string")}} as classificationRanks_classificationId,
+    {{extract_nested_value("classificationRanks","title","string")}} as classificationRanks_title,
+    {{extract_nested_value("classificationRanks","link","string")}} as classificationRanks_link,
+    {{extract_nested_value("classificationRanks","rank","integer")}} as classificationRanks_rank,
+    {{extract_nested_value("displayGroupRanks","websiteDisplayGroup","string")}} as displayGroupRanks_websiteDisplayGroup,
+    {{extract_nested_value("displayGroupRanks","title","string")}} as displayGroupRanks_title,
+    {{extract_nested_value("displayGroupRanks","link","string")}} as displayGroupRanks_link,
+    {{extract_nested_value("displayGroupRanks","rank","integer")}} as displayGroupRanks_rank,
+    {{extract_nested_value("summaries","marketplaceId","string")}} as summaries_marketplaceId,
+    coalesce({{extract_nested_value("summaries","brand","string")}},'N/A') as summaries_brandName,
+    {{extract_nested_value("summaries","color","string")}} as summaries_colorName,
+    {{extract_nested_value("summaries","itemClassification","string")}} as summaries_itemClassification,
+    {{extract_nested_value("summaries","itemName","string")}} as summaries_itemName,
+    {{extract_nested_value("summaries","manufacturer","string")}} as summaries_manufacturer,
+    coalesce({{extract_nested_value("summaries","modelNumber","string")}},'N/A') as summaries_modelNumber,
+    {{extract_nested_value("summaries","packageQuantity","integer")}} as summaries_packageQuantity,
+    {{extract_nested_value("summaries","partNumber","string")}} as summaries_partNumber,
+    {{extract_nested_value("summaries","size","string")}} as summaries_sizeName,
+    {{extract_nested_value("summaries","style","string")}} as summaries_styleName,
+    {{extract_nested_value("summaries","websiteDisplayGroup","string")}} as summaries_websiteDisplayGroup,
+    {{extract_nested_value("summaries","websiteDisplayGroupName","string")}} as summaries_websiteDisplayGroupName,
+    {{daton_user_id()}} as _daton_user_id,
+    {{daton_batch_runtime()}} as _daton_batch_runtime,
+    {{daton_batch_id()}} as _daton_batch_id,
+    current_timestamp() as _last_updated,
+    '{{env_var("DBT_CLOUD_RUN_ID", "manual")}}' as _run_id
+    from {{i}} a
+        {{unnesting("summaries")}}
+        {{unnesting("salesRanks")}}
+        {{multi_unnesting("salesRanks","classificationRanks")}}
+        {{multi_unnesting("salesRanks","displayGroupRanks")}}
+        {% if is_incremental() %}
+            {# /* -- this filter will only be applied on an incremental run */ #}
+            where {{daton_batch_runtime()}}  >= (select coalesce(max(_daton_batch_runtime) - {{ var('CatalogItems_lookback') }},0) from {{ this }})
         {% endif %}
-
-        {% if var('get_storename_from_tablename_flag') %}
-            {% set store =i.split('.')[2].split('_')[var('storename_position_in_tablename')] %}
-        {% else %}
-            {% set store = var('default_storename') %}
-        {% endif %}
-
-        {% if var('timezone_conversion_flag') and i.lower() in tables_lowercase_list and i in var('raw_table_timezone_offset_hours') %}
-            {% set hr = var('raw_table_timezone_offset_hours')[i] %}
-        {% else %}
-            {% set hr = 0 %}
-        {% endif %}
-
-            select 
-            '{{brand}}' as brand,
-            '{{store}}' as store,
-            cast({{ dbt.dateadd(datepart="hour", interval=hr, from_date_or_timestamp="cast(RequeststartDate as timestamp)") }} as {{ dbt.type_timestamp() }}) as RequeststartDate,
-            cast({{ dbt.dateadd(datepart="hour", interval=hr, from_date_or_timestamp="cast(RequestendDate as timestamp)") }} as {{ dbt.type_timestamp() }}) as RequestendDate,
-            coalesce(ReferenceASIN,'N/A') as ReferenceASIN,
-            sellingPartnerId,
-            marketplaceName,
-            a.marketplaceId,
-            asin,
-            {{extract_nested_value("salesRanks","marketplaceId","string")}} as salesRanks_marketplaceId,
-            {{extract_nested_value("classificationRanks","classificationId","string")}} as classificationRanks_classificationId,
-            {{extract_nested_value("classificationRanks","title","string")}} as classificationRanks_title,
-            {{extract_nested_value("classificationRanks","link","string")}} as classificationRanks_link,
-            {{extract_nested_value("classificationRanks","rank","integer")}} as classificationRanks_rank,
-            {{extract_nested_value("displayGroupRanks","websiteDisplayGroup","string")}} as displayGroupRanks_websiteDisplayGroup,
-            {{extract_nested_value("displayGroupRanks","title","string")}} as displayGroupRanks_title,
-            {{extract_nested_value("displayGroupRanks","link","string")}} as displayGroupRanks_link,
-            {{extract_nested_value("displayGroupRanks","rank","integer")}} as displayGroupRanks_rank,
-            {{extract_nested_value("summaries","marketplaceId","string")}} as summaries_marketplaceId,
-            coalesce({{extract_nested_value("summaries","brand","string")}},'N/A') as summaries_brandName,
-            {{extract_nested_value("summaries","color","string")}} as summaries_colorName,
-            {{extract_nested_value("summaries","itemClassification","string")}} as summaries_itemClassification,
-            {{extract_nested_value("summaries","itemName","string")}} as summaries_itemName,
-            {{extract_nested_value("summaries","manufacturer","string")}} as summaries_manufacturer,
-            coalesce({{extract_nested_value("summaries","modelNumber","string")}},'N/A') as summaries_modelNumber,
-            {{extract_nested_value("summaries","packageQuantity","integer")}} as summaries_packageQuantity,
-            {{extract_nested_value("summaries","partNumber","string")}} as summaries_partNumber,
-            {{extract_nested_value("summaries","size","string")}} as summaries_sizeName,
-            {{extract_nested_value("summaries","style","string")}} as summaries_styleName,
-            {{extract_nested_value("summaries","websiteDisplayGroup","string")}} as summaries_websiteDisplayGroup,
-            {{extract_nested_value("summaries","websiteDisplayGroupName","string")}} as summaries_websiteDisplayGroupName,
-        	{{daton_user_id()}} as _daton_user_id,
-            {{daton_batch_runtime()}} as _daton_batch_runtime,
-            {{daton_batch_id()}} as _daton_batch_id,
-            current_timestamp() as _last_updated,
-            '{{env_var("DBT_CLOUD_RUN_ID", "manual")}}' as _run_id
-    	    from {{i}} a
-                {{unnesting("summaries")}}
-                {{unnesting("salesRanks")}}
-                {{multi_unnesting("salesRanks","classificationRanks")}}
-                {{multi_unnesting("salesRanks","displayGroupRanks")}}
-                {% if is_incremental() %}
-                {# /* -- this filter will only be applied on an incremental run */ #}
-                where {{daton_batch_runtime()}}  >= {{max_loaded}}
-                {% endif %}   
-            qualify row_number() over (partition by {{extract_nested_value("summaries","brand","string")}},ReferenceASIN,{{extract_nested_value("summaries","modelNumber","string")}},{{extract_nested_value("summaries","marketplaceId","string")}} order by {{daton_batch_runtime()}} desc, {{daton_batch_id()}} desc) = 1
-        {% if not loop.last %} union all {% endif %}
-    {% endfor %}
+        qualify row_number() over (partition by {{extract_nested_value("summaries","brand","string")}},ReferenceASIN,{{extract_nested_value("summaries","modelNumber","string")}},{{extract_nested_value("summaries","marketplaceId","string")}} order by {{daton_batch_runtime()}} desc, {{daton_batch_id()}} desc) = 1
+    {% if not loop.last %} union all {% endif %}
+{% endfor %}
--- models/AmazonSellerCentral/FBAAmazonFulfilledShipmentsReport.sql
+++ models/AmazonSellerCentral/FBAAmazonFulfilledShipmentsReport.sql
@@ -8,138 +8,109 @@
 -- depends_on: {{ref('ExchangeRates')}}
 {% endif %}
 
-    {% if is_incremental() %}
-    {%- set max_loaded_query -%}
-    select coalesce(max(_daton_batch_runtime) - 2592000000,0) from {{ this }}
-    {% endset %}
 
-    {%- set max_loaded_results = run_query(max_loaded_query) -%}
+{% set relations = dbt_utils.get_relations_by_pattern(
+schema_pattern=var('raw_schema'),
+table_pattern=var('FBAAmazonFulfilledShipmentsReport_tbl_ptrn'),
+exclude=var('FBAAmazonFulfilledShipmentsReport_tbl_exclude_ptrn'),
+database=var('raw_database')) %}
 
-    {%- if execute -%}
-    {% set max_loaded = max_loaded_results.rows[0].values()[0] %}
+{% for i in relations %}
+    {% if var('get_brandname_from_tablename_flag') %}
+        {% set brand =replace(i,'`','').split('.')[2].split('_')[var('brandname_position_in_tablename')] %}
     {% else %}
-    {% set max_loaded = 0 %}
-    {%- endif -%}
+        {% set brand = var('default_brandname') %}
     {% endif %}
 
-    {% set table_name_query %}
-    {{set_table_name('%fulfilledshipments%')}}    
-    {% endset %} 
-
-
-    {% set results = run_query(table_name_query) %}
-    {% if execute %}
-        {# Return the first column #}
-        {% set results_list = results.columns[0].values() %}
-        {% set tables_lowercase_list = results.columns[1].values() %}
+    {% if var('get_storename_from_tablename_flag') %}
+        {% set store =replace(i,'`','').split('.')[2].split('_')[var('storename_position_in_tablename')] %}
     {% else %}
-        {% set results_list = [] %}
-        {% set tables_lowercase_list = [] %}
+        {% set store = var('default_storename') %}
     {% endif %}
 
-    {% for i in results_list %}
-        {% if var('get_brandname_from_tablename_flag') %}
-            {% set brand =i.split('.')[2].split('_')[var('brandname_position_in_tablename')] %}
+    select *, row_number() over (partition by purchase_date, sku, amazon_order_id order by _daton_batch_runtime, quantity_shipped) as _seq_id
+    from (
+        select 
+        '{{brand|replace("`","")}}' as brand,
+        '{{store|replace("`","")}}' as store,
+        {{ timezone_conversion("ReportstartDate") }} as ReportstartDate,
+        {{ timezone_conversion("ReportendDate") }} as ReportendDate,
+        {{ timezone_conversion("ReportRequestTime") }} as ReportRequestTime,
+        sellingPartnerId,
+        marketplaceName,
+        marketplaceId,
+        coalesce(amazon_order_id,'N/A') as amazon_order_id,
+        merchant_order_id,
+        shipment_id,
+        shipment_item_id,
+        amazon_order_item_id,
+        merchant_order_item_id,
+        {{ timezone_conversion("purchase_date") }} as purchase_date,
+        {{ timezone_conversion("payments_date") }} as payments_date,
+        {{ timezone_conversion("shipment_date") }} as shipment_date,
+        {{ timezone_conversion("reporting_date") }} as reporting_date,
+        buyer_email,
+        buyer_name,
+        buyer_phone_number,
+        coalesce(sku,'N/A') as sku,
+        product_name,
+        quantity_shipped,
+        currency,
+        item_price,
+        item_tax,
+        shipping_price,
+        shipping_tax,
+        gift_wrap_price,
+        gift_wrap_tax,
+        ship_service_level,
+        recipient_name,
+        ship_address_1,
+        ship_address_2,
+        ship_address_3,
+        ship_city,
+        ship_state,
+        ship_postal_code,
+        ship_country,
+        ship_phone_number,
+        bill_address_1,
+        bill_address_2,
+        bill_address_3,
+        bill_city,
+        bill_state,
+        bill_postal_code,
+        bill_country,
+        item_promotion_discount,
+        ship_promotion_discount,
+        carrier,
+        tracking_number,
+        estimated_arrival_date,
+        fulfillment_center_id,
+        fulfillment_channel,
+        sales_channel,
+        {% if var('currency_conversion_flag') %}
+            case when c.value is null then 1 else c.value end as exchange_currency_rate,
+            case when c.from_currency_code is null then a.currency else c.from_currency_code end as exchange_currency_code,
         {% else %}
-            {% set brand = var('default_brandname') %}
-        {% endif %}
-
-        {% if var('get_storename_from_tablename_flag') %}
-            {% set store =i.split('.')[2].split('_')[var('storename_position_in_tablename')] %}
-        {% else %}
-            {% set store = var('default_storename') %}
-        {% endif %}
-
-        {% if var('timezone_conversion_flag') and i.lower() in tables_lowercase_list and i in var('raw_table_timezone_offset_hours') %}
-            {% set hr = var('raw_table_timezone_offset_hours')[i] %}
-        {% else %}
-            {% set hr = 0 %}
-        {% endif %}
-
-
-        select *, row_number() over (partition by purchase_date, sku, amazon_order_id order by _daton_batch_runtime, quantity_shipped) as _seq_id
-        from (
-            select 
-            '{{brand}}' as brand,
-            '{{store}}' as store,
-            cast({{ dbt.dateadd(datepart="hour", interval=hr, from_date_or_timestamp="cast(ReportstartDate as timestamp)") }} as {{ dbt.type_timestamp() }}) as ReportstartDate,
-            cast({{ dbt.dateadd(datepart="hour", interval=hr, from_date_or_timestamp="cast(ReportendDate as timestamp)") }} as {{ dbt.type_timestamp() }}) as ReportendDate,
-            cast({{ dbt.dateadd(datepart="hour", interval=hr, from_date_or_timestamp="cast(ReportRequestTime as timestamp)") }} as {{ dbt.type_timestamp() }}) as ReportRequestTime,
-            sellingPartnerId,
-            marketplaceName,
-            marketplaceId,
-            coalesce(amazon_order_id,'N/A') as amazon_order_id,
-            merchant_order_id,
-            shipment_id,
-            shipment_item_id,
-            amazon_order_item_id,
-            merchant_order_item_id,
-            cast({{ dbt.dateadd(datepart="hour", interval=hr, from_date_or_timestamp="cast(purchase_date as timestamp)") }} as {{ dbt.type_timestamp() }}) as purchase_date,
-            cast({{ dbt.dateadd(datepart="hour", interval=hr, from_date_or_timestamp="cast(payments_date as timestamp)") }} as {{ dbt.type_timestamp() }}) as payments_date,
-            cast({{ dbt.dateadd(datepart="hour", interval=hr, from_date_or_timestamp="cast(shipment_date as timestamp)") }} as {{ dbt.type_timestamp() }}) as shipment_date,
-            cast({{ dbt.dateadd(datepart="hour", interval=hr, from_date_or_timestamp="cast(reporting_date as timestamp)") }} as {{ dbt.type_timestamp() }}) as reporting_date,
-            buyer_email,
-            buyer_name,
-            buyer_phone_number,
-            coalesce(sku,'N/A') as sku,
-            product_name,
-            quantity_shipped,
-            currency,
-            item_price,
-            item_tax,
-            shipping_price,
-            shipping_tax,
-            gift_wrap_price,
-            gift_wrap_tax,
-            ship_service_level,
-            recipient_name,
-            ship_address_1,
-            ship_address_2,
-            ship_address_3,
-            ship_city,
-            ship_state,
-            ship_postal_code,
-            ship_country,
-            ship_phone_number,
-            bill_address_1,
-            bill_address_2,
-            bill_address_3,
-            bill_city,
-            bill_state,
-            bill_postal_code,
-            bill_country,
-            item_promotion_discount,
-            ship_promotion_discount,
-            carrier,
-            tracking_number,
-            estimated_arrival_date,
-            fulfillment_center_id,
-            fulfillment_channel,
-            sales_channel,
+            cast(1 as decimal) as exchange_currency_rate,
+            a.currency as exchange_currency_code, 
+        {% endif %} 
+        a.{{daton_user_id()}} as _daton_user_id,
+        a.{{daton_batch_runtime()}} as _daton_batch_runtime,
+        a.{{daton_batch_id()}} as _daton_batch_id,
+        current_timestamp() as _last_updated,
+        '{{env_var("DBT_CLOUD_RUN_ID", "manual")}}' as _run_id
+        from {{i}} a
             {% if var('currency_conversion_flag') %}
-                case when c.value is null then 1 else c.value end as exchange_currency_rate,
-                case when c.from_currency_code is null then a.currency else c.from_currency_code end as exchange_currency_code,
-            {% else %}
-                cast(1 as decimal) as exchange_currency_rate,
-                a.currency as exchange_currency_code, 
-            {% endif %} 
-            a.{{daton_user_id()}} as _daton_user_id,
-            a.{{daton_batch_runtime()}} as _daton_batch_runtime,
-            a.{{daton_batch_id()}} as _daton_batch_id,
-            current_timestamp() as _last_updated,
-            '{{env_var("DBT_CLOUD_RUN_ID", "manual")}}' as _run_id
-            from {{i}} a
-                {% if var('currency_conversion_flag') %}
-                        left join {{ref('ExchangeRates')}} c on date(a.purchase_date) = c.date and a.currency = c.to_currency_code
-                {% endif %}
-                {% if is_incremental() %}
+                    left join {{ref('ExchangeRates')}} c on date(a.purchase_date) = c.date and a.currency = c.to_currency_code
+            {% endif %}
+            {% if is_incremental() %}
                 {# /* -- this filter will only be applied on an incremental run */ #}
-                where a.{{daton_batch_runtime()}}  >= {{max_loaded}}
+                where {{daton_batch_runtime()}}  >= (select coalesce(max(_daton_batch_runtime) - {{ var('FBAAmazonFulfilledShipmentsReport_lookback') }},0) from {{ this }})
                 and sales_channel!='Non-Amazon'
-                {% else %}
+            {% else %}
                 where sales_channel!='Non-Amazon'
-                {% endif %}     
-            qualify row_number() over (partition by purchase_date, sku, amazon_order_id,marketplaceName order by a.{{daton_batch_runtime()}} desc) = 1
-        )
-        {% if not loop.last %} union all {% endif %}
-    {% endfor %}
+            {% endif %}     
+        qualify row_number() over (partition by purchase_date, sku, amazon_order_id,marketplaceName order by a.{{daton_batch_runtime()}} desc) = 1
+    )
+    {% if not loop.last %} union all {% endif %}
+{% endfor %}
